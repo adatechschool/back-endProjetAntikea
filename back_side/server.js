5565d335d602d231973a303493cf07fa026c7f13
accardigianni <gianni.accardi@epitech.eu>
2023-06-27 16:27:27 +0200

    getbeck of 1 day

--- back_side/server.js
+++ back_side/server.js
@@ -4,11 +4,24 @@ const app = express()
 // les layouts mais peut être du front ?
 const expressLayouts = require('express-ejs-layouts')
 
+const indexRouter = require('./routes/index')
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+}).then(() => {
+    console.log('Connexion à la base de données réussie');
+}).catch((error) => {
+    console.error('Erreur lors de la connexion à la base de données :', error);
+});
+
+
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
