21eb9006878e52b013064c7ecaf37eaeb21530d5
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-27 15:04:12 +0200

    comment : server and routes

--- back_side/routes/index.js
+++ back_side/routes/index.js
@@ -1,8 +1,15 @@
+// ---ROUTES for index.html---
+
+// import library express in this file
 const express = require('express')
+// method Router allows us to create routes
 const router = express.Router()
 
+// GET route for basic url
 router.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+
+// export router 
 module.exports = router
--- back_side/server.js
+++ back_side/server.js
@@ -1,18 +1,27 @@
-const express = require('express')
-const app = express()
 
-// les layouts mais peut être du front ?
+// import express library and stokc it in a variable 
+const express = require('express')
+// intance of express : "app", its our application !
+const app = express() //=server
+
+// layouts to avoid code duplication - maybe unusefull 
 const expressLayouts = require('express-ejs-layouts')
 
+// import router
 const indexRouter = require('./routes/index')
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
+// server settings
+app.set('view engine', 'ejs')  // set server views 
+app.set('views', __dirname + '/views') // path views
+app.set('layout', 'layouts/layout')  // layouts to display 
 
+// server tools
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+// use the route
 app.use('/', indexRouter)
 
+
+// server start nad listen on a specify port
 app.listen(process.env.PORT || 3000)
