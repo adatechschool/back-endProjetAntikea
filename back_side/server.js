c93cf66c10819f1a48e7b6a6a4190c63a66a382f
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-27 12:37:43 +0200

    feat : first route

--- back_side/server.js
+++ back_side/server.js
@@ -4,11 +4,15 @@ const app = express()
 // les layouts mais peut être du front ?
 const expressLayouts = require('express-ejs-layouts')
 
+const indexRouter = require('./routes/index')
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
