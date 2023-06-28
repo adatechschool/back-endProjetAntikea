e68deea58e312a4cfdc6d46c84647699bb5f3a88
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-06-28 15:56:13 +0200

    suppression commits

--- front_side/meuble-app/src/App.js
+++ front_side/meuble-app/src/App.js
@@ -1,12 +1,2 @@
-import React from "react";
-
-function App() {
-  return (
-    <div>
-      <h1>Bienvenue dans mon application React !</h1>
-      <p>C'est un exemple de code.</p>
-    </div>
-  );
-}
 
 export default App;
