596474a40426ca67cf7cab2e3fa5d563dc5fce06
LPF-Quentin <quentin-rousseau@orange.fr>
2023-06-28 14:11:09 +0200

    commit

--- front_side/meuble-app/src/App.js
+++ front_side/meuble-app/src/App.js
@@ -1,4 +1,7 @@
-function App() {
+import React from "react";
+import Footer from "./components/Footer/Footer";
+
+// function App() {
   //state
   
 
@@ -8,6 +11,14 @@ function App() {
   //render(affichage)
 
 
-}
+// }
+
+const App = () => (
+  <div>
+    <h3>GeeksforGeeks</h3>
+    <h2>Sticky Footer using Reactjs!</h2>
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ front_side/meuble-app/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => {
+    // event.preventDefault();
+    return (
+    <Footer>
+        <h1>ceci est un footer exeptionnel</h1>
+        
+    </Footer>
+)
+}
+
+export default Footer;
