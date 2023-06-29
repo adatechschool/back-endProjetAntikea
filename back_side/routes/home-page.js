32442f81c6928df0df9e82666a74c56abdc111a2
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-29 18:31:07 +0200

    features: meubles route put and delete

--- back_side/routes/home-page.js
+++ back_side/routes/home-page.js
@@ -5,12 +5,29 @@ const express = require('express')
 // method Router allows us to create routes
 const router = express.Router()
 
+// import mongoose library to get methods usefull to manipulate database
 const mongoose = require("mongoose")
 
+// import a meuble schema and change it to a model than can be manipulate
 const meubleSchema = require("../models/meuble")
 const meubleModel = mongoose.model('Meuble', meubleSchema)
 
-// --- New Route :create the route to display 5 last fourniture to sell on the home page ---
+
+// --- New Routes to manipulate meubles collection ---
+
+// GET a specific furniture
+router.get('/:id', (req, res, next) => {
+    meubleModel.findOne({ _id: req.params.id })
+    .then(meuble => res.status(200).json(meuble))
+    .catch(error => res.status(404).json({ error }))
+})
+
+router.put("/id", (req, res, next) => {
+    meubleModel.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Meuble modifié'}))
+    .catch(error => res.status(400).json({ error }))
+})
+
 // GET to obtain the info from the "meubles" table
 router.get('/', (req, res) => {
     meubleModel.find()
@@ -19,25 +36,28 @@ router.get('/', (req, res) => {
         )
         .catch(err => {
             console.log(err)
-            .then((() => res.status(201).json({ message: "Objet enregistré"})))
+            .then((() => res.status(201).json({ message: "Meuble enregistré"})))
             .catch(error => res.status(400).json({ error }))
         })
 })
 
+// POST to add a furniture in the data base
 router.post('/', (req, res) => {
     delete req.body._id; //s'il y a déjà un id dans la requete
     const addMeuble = new meubleModel({
         ...req.body
     })
     addMeuble.save()
+})
+
+// DELETE un meuble
+router.delete('/:id', (req, res) => {
+    meubleModel.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Meuble supprimé'}))
+    .catch(error => res.status(400).json({ error }))
 
 })
 
-router.get('/:id', (req, res, next) => {
-    meubleModel.findOne({ _id: req.params.id })
-    .then(meuble => res.status(200).json(meuble))
-    .catch(error => res.status(404).json({ error }))
-})
 
 // export router 
 module.exports = router
