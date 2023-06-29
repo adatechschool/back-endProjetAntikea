702be8cbbb843220a54f54223ebf24480b01e07b
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-06-29 16:55:09 +0200

    Sorting component OK

--- front_side/meuble-app/src/App.js
+++ front_side/meuble-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "./components/button";
 import CardsProduits from "./components/CardsProduits/CardsProduits";
+import Sorting from "./components/Sorting/Sorting.js";
 
 function App() {
   return <div></div>;
