db5caab83e209bbabd7e7aabed9d3d906fa4570c
LPF-Quentin <quentin-rousseau@orange.fr>
2023-06-30 12:10:25 +0200

    correction du css de footer

--- front_side/meuble-app/src/components/Footer/Footer.js
+++ front_side/meuble-app/src/components/Footer/Footer.js
@@ -4,13 +4,13 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer>
-            <div class="column" id="norm">
+            <div className="column" id="norm">
                 <h1>ANTIKEA</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur<br />adipiscing elit<br/>
                 3 bd Stalingrad, 44000<br />Nantes</p>
                 <a href="#">contact@antikea.fr</a>
             </div>
-            <div class="column" id="shopping">
+            <div className="column" id="shopping">
                 <h1 id="textFooter">SHOPPING</h1>
                 <ul>
                     <li id="lien"><a href="#">Votre panier</a></li>
@@ -19,14 +19,14 @@ const Footer = () => {
                     <li id="lien"><a href="#">Livraison</a></li>
                 </ul>
             </div>
-            <div class="column" id="liens">
+            <div className="column" id="liens">
                 <h1 id="textFooter">PLUS DE LIENS</h1>
                 <ul>
                     <li id="lien"><a href="#">Nouveautés</a></li>
                     <li id="lien"><a href="#">Promotions</a></li>
                 </ul>
             </div>
-            <div class="column" id="events">
+            <div className="column" id="events">
                 <h1 id="textFooter">EVENEMENTS</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />Repellat a alias recusandae. Quis temporibus, in nisi impedit quisquam sunt odio deleniti quaerat</p>
             </div>
