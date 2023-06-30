230fd9b07575a016000190f8a3f8bade7a5e367e
MarionLpz <123973628+MarionLpz@users.noreply.github.com>
2023-06-30 15:41:52 +0200

    Add logo in footer

--- front_side/meuble-app/src/components/Footer/Footer.js
+++ front_side/meuble-app/src/components/Footer/Footer.js
@@ -1,42 +1,61 @@
 import React from "react";
-import logo from '../Header/logo.jpg';
-
-import './Footer.css'
+import logo from "./logo.png";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="column" id="norm">
-                <h1>ANTIKEA</h1>
-                <img class="logo" alt="logo" src={logo} />
-                <p>Lorem ipsum dolor sit amet, consectetur<br />adipiscing elit<br/>
-                3 bd Stalingrad, 44000<br />Nantes</p>
-                <a href="#">contact@antikea.fr</a>
-            </div>
-            <div className="column" id="shopping">
-                <h1 id="textFooter">SHOPPING</h1>
-                <ul>
-                    <li id="lien"><a href="#">Votre panier</a></li>
-                    <li id="lien"><a href="#">Vos commandes</a></li>
-                    <li id="lien"><a href="#">Vos favoris</a></li>
-                    <li id="lien"><a href="#">Livraison</a></li>
-                </ul>
-            </div>
-            <div className="column" id="liens">
-                <h1 id="textFooter">PLUS DE LIENS</h1>
-                <ul>
-                    <li id="lien"><a href="#">Nouveautés</a></li>
-                    <li id="lien"><a href="#">Promotions</a></li>
-                </ul>
-            </div>
-            <div className="column" id="events">
-                <h1 id="textFooter">EVENEMENTS</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />Repellat a alias recusandae. Quis temporibus, in nisi impedit quisquam sunt odio deleniti quaerat</p>
-            </div>
-        </footer>
+  return (
+    <footer>
+      <div className="column" id="norm">
+        <img src={logo} class="logo2" alt="logo" />
+        <p>
+          Fabriqué, avec amour et résilience. Beaucoup de résilience.
+          <br />
+          A Ada Tech School
+          <br />
+          3 bd Stalingrad, 44000
+          <br />
+          Nantes
+        </p>
+        <a href="#">contact@antikea.fr</a>
+      </div>
+      <div className="column" id="shopping">
+        <h1 id="textFooter">SHOPPING</h1>
+        <ul>
+          <li id="lien">
+            <a href="#">Votre panier</a>
+          </li>
+          <li id="lien">
+            <a href="#">Vos commandes</a>
+          </li>
+          <li id="lien">
+            <a href="#">Vos favoris</a>
+          </li>
+          <li id="lien">
+            <a href="#">Livraison</a>
+          </li>
+        </ul>
+      </div>
+      <div className="column" id="liens">
+        <h1 id="textFooter">PLUS DE LIENS</h1>
+        <ul>
+          <li id="lien">
+            <a href="#">Nouveautés</a>
+          </li>
+          <li id="lien">
+            <a href="#">Promotions</a>
+          </li>
+        </ul>
+      </div>
+      <div className="column" id="events">
+        <h1 id="textFooter">EVENEMENTS</h1>
+        <p>
+          Un site de Aïssata Diallo, Alissia Leroy, Elsa Catoire, Gianni
+          Accardi, Quentin Rousseau, Marion Lopez, Meghan Grousset et Sébastien
+          Fougère.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-
-    );
-}
-
-export default Footer
+export default Footer;
