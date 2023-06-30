2e102503a47b70ca2cbb5f2b71826420ce6d8588
SebFoodJ <sebastien.fougere@gmail.com>
2023-06-30 11:32:43 +0200

    Modif Header et Routes sur App

--- front_side/meuble-app/src/App.js
+++ front_side/meuble-app/src/App.js
@@ -2,18 +2,28 @@ import React from "react";
 import Connexion from "./components/Connection/Connexion";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import CardsProduits from "./components/CardsProduits/CardsProduits"
 // import BasicExample from './components/Connection/Connexion';
 import Sorting from "./components/Sorting/Sorting.js";
+// Import des Routes
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div id="app">
+      <Router>
       {/* Autres composants et contenu de l'application */}
-      <Header />
-      <Connexion />
-      <Footer />
+        <Header />
+          <Routes>
+            <Route path="/main" component={Sorting} />
+            <Route path="/main" component={CardsProduits} />
+            <Route path="/connexion" component={Connexion} />
+          </Routes>
+        <Footer />
       {/* <Button /> */}
+      </Router>
     </div>
+    
   );
 }
 
--- front_side/meuble-app/src/components/Header/Header.js
+++ front_side/meuble-app/src/components/Header/Header.js
@@ -2,17 +2,20 @@ import React from 'react';
 import logo from './logo.jpg';
 import user from './user.png';
 import basket from './basket-shopping-simple.png';
+import {Link} from "react-router-dom"
 import './Header.css';
 
 function Header() {
   return (
     <header className="header">
       <img className="logo" alt="" src={logo} />
-      <div className="group-parent">
-        <img className="icon-user" alt="" src={user} />
-        <img className="icon-basket" alt="" src={basket} />
-        <div className="un-meuble-a-vendre-parent">
-          <b className="un-meuble-a">UN MEUBLE A VENDRE ?</b>
+      <div className="navbar">
+        <div className="icons">
+          <img className="icon-user" alt="user" src={user} />
+          <img className="icon-basket" alt="basket" src={basket} />
+        </div>
+        <div className="liens">
+          <b className="a-vendre">UN MEUBLE A VENDRE ?</b>
           <b className="acceuil">ACCUEIL</b>
           <b className="nous-contacter">NOUS CONTACTER</b>
         </div>
