1303ceff1c229676c466653bb533279a50cc4fd7
Elsa Catoire <elsa.catoire@gmail.com>
2023-06-30 11:15:22 +0200

    update : change of file name for meubles route

--- back_side/server.js
+++ back_side/server.js
@@ -16,8 +16,7 @@ const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
 //login
 const loginRouter = require('./routes/login')
-const homePageRouter = require('./routes/home-page')
-
+const meublesRouter = require('./routes/meubles')
 // server settings
 app.set('view engine', 'ejs')  // set server views 
 app.set('views', __dirname + '/views') // path views
@@ -44,10 +43,21 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))*/
 
+/* // setting up our database if its the online one. DO NOT USE
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    // log if we are connected or not to the database
+    .then(() => {
+        console.log('Connexion à la base de données réussie')
+    })
+    .catch((error) => {
+        console.error('Erreur lors de la connexion à la base de données :', error)
+    }) */
+
 // use the route
 app.use('/', indexRouter)
 app.use('/login', loginRouter)
-app.use('/meubles', homePageRouter)
+app.use('/meubles', meublesRouter)
 
 
 
