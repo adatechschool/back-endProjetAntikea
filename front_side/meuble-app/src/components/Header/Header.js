f68c6fff22a7991010740951ccb15838fad19981
SebFoodJ <sebastien.fougere@gmail.com>
2023-06-30 09:40:16 +0200

    Update Header.js

--- front_side/meuble-app/src/components/Header/Header.js
+++ front_side/meuble-app/src/components/Header/Header.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import logo from './logo.jpg';
 import user from './user.png';
 import basket from'./basket-shopping-simple.png';
@@ -9,7 +6,7 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="header">
+    <header className="header">
       <div className="group-parent">
         <img className="icon-user" alt="" src={user} />
         <img className="icon-basket" alt="" src={basket} />
@@ -20,7 +17,7 @@ function Header() {
         </div>
       </div>
       <img className="logo" alt="" src={logo} />
-    </div>
+    </header>
   );
 }
 
